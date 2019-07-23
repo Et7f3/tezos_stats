3ad8f39884fa9115c069019fb88d40bcc04257bd
Élie BRAMI <elie.brami@epita.fr>
2019-07-23 14:40:54 +0200

    monitor one node

--- src/main.ml
+++ src/main.ml
@@ -1,36 +1,14 @@
-(*
-let () = print_endline "e"
-let a = 4
-let error: EzRequest.error_handler = Printf.(fun i -> function None -> printf " None%d" i | Some s -> printf " %s %d" s i)
-let error: EzRequest.error_handler = Printf.(fun i -> function None -> failwith " None"| Some s -> failwith s)
-let e = ref false
-let mut = Mutex.create ()
-let () = Mutex.lock mut
-let c = Condition.create ()
-let () =
-  EzCohttp.get "load-sponsors"
-    (EzAPI.TYPES.URL "http://api1.tzscan.io/v1/services") ~error
-    (fun s ->
-      let () = print_endline "test" in
-      let () = print_endline s in
-      let () = e := true in
-      let () = Condition.signal c in
-      flush_all ())
-let () = print_endline "e"
-let () = Condition.wait c mut
-*)
-
 type config =
   {
-    nodes: string array;
+    target: string;
   }
 
 let read_config config_file =
   let yojson = Yojson.Safe.from_file ~fname:"config" config_file in
   let ezjsonm = Json_repr.from_yojson yojson in
-  let nodes = Json_encoding.(destruct (obj1 (req "nodes" (array string))) ezjsonm) in
+  let target = Json_encoding.(destruct (obj1 (req "target" (string))) ezjsonm) in
   {
-    nodes
+    target
   }
 
 let config =
@@ -40,41 +18,27 @@ let config =
   (* hack during development *)
   | _ -> read_config "src/config.json"
 
-let () = print_endline "hello"
+let () = print_endline "config loaded"
+let () = print_endline ("We will monitor: " ^ config.target)
 
-let print_head_hash =
-  let error: EzRequest.error_handler = Printf.(fun i -> function None -> printf " None%d" i | Some s -> printf " %s %d" s i) in
-  function root ->
-    let a, b = Lwt.wait () in
-    let url = Printf.sprintf "%s/chains/main/blocks/head/hash" root in
-    let () = Printf.printf "on accede %s\n" url in
+let print_head head =
+  print_endline (config.target ^ " is at " ^ head)
+
+let rec peek_head =
+  let url = EzAPI.TYPES.URL (config.target ^ "/chains/main/blocks/head/hash")
+  and error = fun i -> function
+      Some s -> Printf.eprintf "A request has failed with error n: %s" s
+    | None -> Printf.eprintf "A request has failed with error n"
+  in function () ->
+    let ret, _resolver = Lwt.wait () in
     let () =
-      EzCohttp.get "load-sponsors"
-        (EzAPI.TYPES.URL url) ~error
-        (fun hash ->
-          let () = Printf.printf "head %s: %s\n" root hash in
-          Lwt.wakeup b ())
-    in a
+      EzCohttp.get "peek_head" url ~error
+        (fun s ->
+          let () = print_head s in
+          let sleep = Lwt_unix.sleep 10. in
+          let _ = Lwt.bind sleep peek_head in
+          ())
+    in
+    ret
 
-let () = Array.map print_head_hash config.nodes |> Array.iter Lwt_main.run
-
-(* http://tz.next.tzscan.io/chains/main/blocks/head/hash
-Json_encoding.(destruct string) (Yojson.Safe.from_string s |> Json_repr.from_yojson);;
-*)
-(*
-let a, b = Lwt.wait ()
-let error: EzRequest.error_handler = Printf.(fun i -> function None -> printf " None%d" i | Some s -> printf " %s %d" s i)
-let () =
-  EzCohttp.get "load-sponsors"
-    (EzAPI.TYPES.URL "http://api1.tzscan.io/v1/services") ~error
-    (fun s ->
-      let () = print_endline "test" in
-      let () = print_endline s in
-      let () = flush_all () in
-      let () = print_endline "on va deverouiller" in
-      Lwt.wakeup b 5)
-
-let _ = Lwt_main.run a
-
-let a = 42
-*)
+let () = peek_head () |> Lwt_main.run
