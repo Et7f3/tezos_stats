a07086d9460d76baffbb06e72f9edcb0cdf95ff6
Élie BRAMI <elie.brami@epita.fr>
2019-08-25 04:21:01 +0200

    Add ezjsonm_of_string and compiled js.

--- src/web_client/main.ml
+++ src/web_client/main.ml
@@ -1,3 +1,19 @@
-open Json_encoding
+let ezjsonm_of_string : string -> Json_repr.ezjsonm = fun data ->
+  let json = Js.Json.parseExn data in
+  let rec traverse t =
+    match Js.Json.classify t with
+      JSONArray arr ->
+      let arr = Array.to_list arr in
+      `A (List.map traverse arr)
+    | JSONFalse -> `Bool false
+    | JSONTrue -> `Bool true
+    | JSONNumber n -> `Float n
+    | JSONNull -> `Null
+    | JSONObject o ->
+      let o = Js.Dict.entries o in
+      `O (Array.to_list (Array.map (fun (s, t) -> (s, traverse t)) o))
+    | JSONString s -> `String s
+  in traverse json
+
 let a = Json_encoding.construct
 let () = print_endline "Hello"
