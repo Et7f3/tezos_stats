762fa81e24f154e0d792a920c820d8d3235d17f6
Élie BRAMI <elie.brami@epita.fr>
2019-08-25 17:41:13 +0200

    begin html

--- src/host_web/host_web.ml
+++ src/host_web/host_web.ml
@@ -3,12 +3,13 @@ open Opium.Std
 let a =
   App.empty
   |> middleware (Middleware.static ~local_path:"lib/js/src/web_client" ~uri_prefix:"/static/js" ())
+  |> middleware (Middleware.static ~local_path:"src/web_assets" ~uri_prefix:"/" ())
   |> App.run_command'
 
 let () =
   match a with
     `Ok w ->
-    let () = print_endline "We static js" in
+    let () = print_endline "We static js and html" in
     Lwt_main.run w
   | `Error -> print_endline "error"
   | `Not_running -> print_endline "not running"
