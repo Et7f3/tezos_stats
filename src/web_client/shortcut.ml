b7641a7dd88f687e5c1b6892fcb809de2fbd52f2
Élie BRAMI <elie.brami@epita.fr>
2019-08-25 13:30:12 +0200

    Add shortcut for reason_react.

---
+++ src/web_client/shortcut.ml
@@ -0,0 +1,14 @@
+(**
+   Shortcur fot reason_react
+*)
+
+let div = ReactDOMRe.createDOMElementVariadic "div"
+let p = ReactDOMRe.createDOMElementVariadic "p"
+let button = ReactDOMRe.createDOMElementVariadic "button"
+let table = ReactDOMRe.createDOMElementVariadic "table"
+let thead = ReactDOMRe.createDOMElementVariadic "thead"
+let tbody = ReactDOMRe.createDOMElementVariadic "tbody"
+let tr = ReactDOMRe.createDOMElementVariadic "tr"
+let th = ReactDOMRe.createDOMElementVariadic "th"
+let td = ReactDOMRe.createDOMElementVariadic "td"
+let img = ReactDOMRe.createDOMElementVariadic "img"
